76be62c6be259a3e1340dd55a68d534221cc17af
Laura Thomas <peluchethomasgodos@gmail.com>
2025-05-29 22:30:36 -0600

    Create therapySession.h

---
+++ therapySession.h
@@ -0,0 +1,16 @@
+#ifndef THERAPYSESSION_H
+#define THERAPYSESSION_H
+#include "appointment.h"
+
+class TherapySession{
+    private:
+        string sessionType;
+    public:
+        TherapySession();
+        TherapySession(string, string,int, int, string);
+        string getDetails();
+        string getDetails(bool); //para vista de doctor
+        
+};
+
+#endif
