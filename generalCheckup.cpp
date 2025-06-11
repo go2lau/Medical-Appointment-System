e61055f8eb526a3bb3b452239a3bbb155ba33738
Laura Thomas <peluchethomasgodos@gmail.com>
2025-06-11 00:06:15 -0600

    Update generalCheckup.cpp

--- generalCheckup.cpp
+++ generalCheckup.cpp
@@ -7,23 +7,33 @@ GeneralCheckup::GeneralCheckup(){
     doctorID=0;
     patientID=0;
     appointmentID=0;   
-    basicVitals="";
+    primaryConcern="";
 }
 
-GeneralCheckup::GeneralCheckup(string date_, string time_, int doctorID_, int patientID_, int appointmentID_, string basicVitals_){
+GeneralCheckup::GeneralCheckup(string date_, string time_, int doctorID_, int patientID_, int appointmentID_, string primaryConcern_){
 
     date=date_;
     time=time_;
     doctorID=doctorID_;
     patientID=patientID_;
     appointmentID_=appointmentID_;
-    basicVitals=basicVitals_;
+    primaryConcern=primaryConcern_;
 }
 
-string GeneralCheckup::getDetails(){
-    //pespectiva del paciente
+string GeneralCheckup::getDetails(){//patient perspective
+    string msg;
+    msg= "These are the details of your checkup \n Date: "+date+"\n"+"Time: "+time+"\nID of your doctor: "+to_string(doctorID)+ "\n"+"Appointment ID: "+to_string(appointmentID)+"\n"+"Primary Concern: "+primaryConcern+"\n";
+    return msg;
 }
 
-string GeneralCheckup::getDetails(bool doctor){
-    //perspectiva del doctor
+
+
+string GeneralCheckup::getDetails(bool doctor){//doctor perspective
+    string msg;
+    if (doctor){
+        msg= "These are the details of your checkup \n Date: "+date+"\n"+"Time: "+time+"\nID of your patient: "+to_string(patientID)+ "\n"+"Appointment ID: "+to_string(appointmentID)+"\n"+"Primary Concern: "+primaryConcern+"\n";
+        return msg;
+    }
+    msg= "These are the details of your checkup \n Date: "+date+"\n"+"Time: "+time+"\nID of your doctor: "+to_string(doctorID)+ "\n"+"Appointment ID: "+to_string(appointmentID)+"\n"+"Primary Concern: "+primaryConcern+"\n";
+    return msg;    
 }
