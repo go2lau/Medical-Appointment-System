cc04ade9f748ede6d57a648fa2e073f707346cb7
Laura Thomas <peluchethomasgodos@gmail.com>
2025-06-11 00:08:06 -0600

    Update therapySession.h

--- therapySession.h
+++ therapySession.h
@@ -2,14 +2,14 @@
 #define THERAPYSESSION_H
 #include "appointment.h"
 
-class TherapySession{
-    private:
+class TherapySession:public Appointment{
+    private: 
         string sessionType;
     public:
         TherapySession();
         TherapySession(string, string,int, int, int, string);
-        string getDetails();
-        string getDetails(bool); //para vista de doctor
+        string getDetails() override;
+        string getDetails(bool) override; //para vista de doctor
         
 };
 
